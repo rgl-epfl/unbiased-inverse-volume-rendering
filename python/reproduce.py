0c36c0d2e0ff1b1ee8f204e5601695de5d147bb1
Merlin Nimier-David <merlin.nimier@gmail.com>
2022-07-27 17:01:28 +0200

    Add most remaining optimization configs

--- python/reproduce.py
+++ python/reproduce.py
@@ -43,25 +43,29 @@ def reproduce_optimization_experiments(configs, overwrite=False):
 
 def main():
     base_opt_config = {
-        'n_iter': 1000,
+        'n_iter': 6000,
+        'preview_stride': 250,
         'spp': 16,
         'primal_spp_factor': 64,
         'lr': 5e-3,
         'lr_schedule': Schedule.Last25,
-        'batch_size': None,
-        'render_initial': True,
-        'render_final': True,
-        'preview_stride': 100
+        'batch_size': 32768,
+        'render_initial': False,
+        'render_final': False,
+        'checkpoint_stride': None,
 
-        # 'n_iter': 6000,
-        # 'spp': 16,
-        # 'primal_spp_factor': 64,
-        # 'lr': 5e-3,
-        # 'batch_size': 32768,
-        # 'render_initial': False,
-        # 'render_final': False,
+        'upsample': [0.04, 0.16, 0.36, 0.64],
     }
 
+    # Structure of this dictionary:
+    # Optimization config name: {
+    #    'scene': scene config name,
+    #    'opt': optimization configuration,
+    #    'integrators': {
+    #        Integrator name: dict of per-integrator optimization config
+    #                         overrides (or None if no overrides are needed),
+    #    }
+    # }
     configs = {
         'janga-smoke-sn64': {
             'scene': 'janga-smoke',
@@ -73,13 +77,181 @@ def main():
                     'spp': 4,
                     'primal_spp_factor': 1,
                 },
-                # 'volpathsimple-basic': None,
-                # 'volpathsimple-drt': None,
-                # TODO: support NeRF initiialization
+                'volpathsimple-drt': None,
+                'volpathsimple-basic': None,
             },
         },
+        'janga-smoke-from-nerf-sn64': {
+            'scene': 'janga-smoke-from-nerf',
+            'opt': deepcopy(base_opt_config),
+            'integrators': {
+                'volpathsimple-drt': {
+                    'upsample': None,
+                },
+                'volpathsimple-basic': {
+                    'upsample': None,
+                },
+            },
+        },
+
+        # ----------
+
+        'dust-devil-sn64': {
+            'scene': 'dust-devil',
+            'opt': deepcopy(base_opt_config),
+            'integrators': {
+                'nerf': {
+                    'lr': 5e-3,
+                    'spp': 4,
+                    'primal_spp_factor': 2,
+                },
+                'volpathsimple-drt': {
+                    'lr': 3e-4,
+                },
+                'volpathsimple-basic': {
+                    'lr': 3e-4,
+                },
+            },
+        },
+        'dust-devil-from-nerf-sn64': {
+            'scene': 'dust-devil-from-nerf',
+            'opt': deepcopy(base_opt_config),
+            'integrators': {
+                'volpathsimple-drt': {
+                    'lr': 1e-4,
+                    'upsample': None,
+                },
+                'volpathsimple-basic': {
+                    'lr': 1e-4,
+                    'upsample': None,
+                },
+            },
+        },
+
+        # ----------
+
+        'astronaut-rotated-sn64': {
+            'scene': 'astronaut-rotated',
+            'opt': deepcopy(base_opt_config),
+            # Integrator name => optimization config overrides
+            'integrators': {
+                'nerf': {
+                    'spp': 4,
+                    'primal_spp_factor': 2,
+                },
+                'volpathsimple-drt': None,
+                'volpathsimple-basic': None,
+            },
+        },
+        'astronaut-rotated-from-nerf-sn64': {
+            'scene': 'astronaut-rotated',
+            'opt': deepcopy(base_opt_config),
+            # Integrator name => optimization config overrides
+            'integrators': {
+                'volpathsimple-drt': {
+                    'upsample': None,
+                },
+                'volpathsimple-basic': {
+                    'upsample': None,
+                },
+            },
+        },
+
+        # ----------
+
+        'rover-sn64': {
+            'scene': 'rover',
+            'opt': deepcopy(base_opt_config),
+            # Integrator name => optimization config overrides
+            'integrators': {
+                'nerf': {
+                    'lr': 1e-2,
+                    'spp': 4,
+                    'primal_spp_factor': 2,
+                },
+                'volpathsimple-drt': {
+                    'lr': 5e-2,
+                },
+                'volpathsimple-basic': {
+                    'lr': 5e-2,
+                },
+            },
+        },
+        'rover-from-nerf-sn64': {
+            'scene': 'rover',
+            'opt': deepcopy(base_opt_config),
+            # Integrator name => optimization config overrides
+            'integrators': {
+                'volpathsimple-drt': {
+                    'lr': 1e-2,
+                    'upsample': None,
+                },
+                'volpathsimple-basic': {
+                    'lr': 1e-2,
+                    'upsample': None,
+                },
+            },
+        },
+
+        # ----------
+
+        'tree-2-sn64': {
+            'scene': 'tree-2',
+            'opt': deepcopy(base_opt_config),
+            # Integrator name => optimization config overrides
+            'integrators': {
+                'nerf': {
+                    'lr': 1e-2,
+                    'spp': 4,
+                    'primal_spp_factor': 2,
+                },
+                'volpathsimple-drt': {
+                    'lr': 1e-2,
+                },
+                'volpathsimple-basic': {
+                    'lr': 1e-2,
+                },
+            },
+        },
+        'tree-2-from-nerf-sn64': {
+            'scene': 'tree-2',
+            'opt': deepcopy(base_opt_config),
+            # Integrator name => optimization config overrides
+            'integrators': {
+                'volpathsimple-drt': {
+                    'lr': 1e-2,
+                    'upsample': None,
+                },
+                'volpathsimple-basic': {
+                    'lr': 1e-2,
+                    'upsample': None,
+                },
+            },
+        },
+
+        # ----------
+
+        # 'janga-smoke-sn64-benchmark': {
+        #     'scene': 'janga-smoke',
+        #     'opt': {
+        #         'n_iter': 6000,
+        #         'preview_stride': 50,
+        #         'spp': 16,
+        #         'primal_spp_factor': 64,
+        #         'lr': 5e-3,
+        #         'batch_size': 32768,
+        #         'render_initial': False,
+        #         'render_final': False,
+        #         'upsample': [0.04, 0.16, 0.36, 0.64],
+        #     },
+        #     # Integrator name => optimization config overrides
+        #     'integrators': {
+        #         'volpathsimple-drt': None,
+        #     },
+        # },
     }
-    reproduce_optimization_experiments(configs)
+
+    reproduce_optimization_experiments(configs, overwrite=False)
 
 
 
